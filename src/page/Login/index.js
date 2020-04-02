0150aebb2f7a82d1fa13cdfda690a151ceb81a18
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-04-02 01:49:02 -0300

    creating login screen functionality

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,57 +1,66 @@
-import React , { useState } from 'react';
-import  {FiMail} from 'react-icons/fi';
+import React, { useState } from 'react';
+import { FiMail , FiUnlock, FiLogIn } from 'react-icons/fi';
 import perfilImg from '../../asset/perfil.jpg';
+
+import loginAcess from '../../utils/loginAcess';
 
 import './styles.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    const dados = {
-      
+    const data = {
       email,
       password
-    };
+    }
 
-    alert(`Seu Email é : ${ dados.email}  Senha: ${dados.password}` )
+    if ( data.email === loginAcess.email && data.password === loginAcess.password){
+      alert(`Acesso Liberado `+ data.email);
+    } else {
+      alert("Acesso negado , se cadastre-se")
+    }
   }
   return (
-    <div className="logon-container">
-      <section className="form">        
+    <div className="logon-container" >
+      <section className="form">
         <img src={perfilImg} alt="perfil"/>
-        
-        <form onSubmit={handleSubmit}>
-          <label for="Register"><FiMail className="iconeMail" size={20} /></label>
 
-          <input 
+        <form onSubmit={handleSubmit}>
+          <h1>
+          <FiMail className="fiIcons" size={24} color="#f3403b"/>
+          </h1>
+          <input
             type="email"
             placeholder="E-mail"
             className="inputLogin"
             value={email}
             onChange={event => setEmail(event.target.value)}
-          ></input>
-
-          <input 
-            placeholder="Password" 
+          />
+          <h1>
+          <FiUnlock className="fiIcons" size={24} color="#f3403b"/>
+          </h1>
+          <input
+            type="password"
+            placeholder="Password"
             className="inputLogin"
             value={password}
             onChange={event => setPassword(event.target.value)}
           />
 
-          <input type="checkbox" name="Register"/>
+          <input type="checkbox" name="Register" />
           <label for="Register">Register Name</label>
 
           <label className="ultimo">Forgout Password</label>
-
+          <FiLogIn className="fiLogin" size={24} color="#f3403b"/>
           <button className="button" type="submit">Login</button>
         </form>
-      
+
       </section>
-    </div>
+    </div >
   );
 }
---
+++ src/utils/loginAcess.js
@@ -0,0 +1,6 @@
+const loginAcess = {
+  email: "admin@admin.com.br",
+  password: "123456"
+}
+
+export default loginAcess;
